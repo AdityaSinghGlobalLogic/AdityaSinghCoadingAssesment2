7bd67858a613832396205bb832d6f91c0537c077
Aditya Singh <110605212+AdityaSinghGlobalLogic@users.noreply.github.com>
2022-08-16 11:15:07 +0530

    Add files via upload

---
+++ Que2/employee/EmployeeEntity.java
@@ -0,0 +1,89 @@
+package com.java.jpa.employee;
+
+import javax.persistence.*;
+
+@Entity
+
+@Table(name="EmployeeDetails")//Table name
+
+public class EmployeeEntity {
+
+  @Id
+
+  private int empno;//Primary Key
+
+  private String name;
+
+  private String designation;
+  private Double sal;
+
+
+
+  public EmployeeEntity(int empno, String name, String designation, Double sal) {
+	super();
+	this.empno = empno;
+	this.name = name;
+	this.designation = designation;
+	this.sal = sal;
+}
+
+
+
+public EmployeeEntity() {
+
+    super();
+
+  }
+
+
+
+public int getEmpno() {
+	return empno;
+}
+
+
+
+public void setEmpno(int empno) {
+	this.empno = empno;
+}
+
+
+
+public String getName() {
+	return name;
+}
+
+
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+
+public String getDesignation() {
+	return designation;
+}
+
+
+
+public void setDesignation(String designation) {
+	this.designation = designation;
+}
+
+
+
+public Double getSal() {
+	return sal;
+}
+
+
+
+public void setSal(double i) {
+	this.sal = i;
+}
+
+
+
+}
+
